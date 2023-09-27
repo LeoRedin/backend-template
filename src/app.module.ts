3fd7be20a9792807e0aa246ad2747d26b4232f27
Leonardo Redin <redin@hey.com>
2023-09-26 23:41:50 -0300

    finished initial setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SlonikModule } from 'nestjs-slonik';
+import { RequestContextModule } from 'nestjs-request-context';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { UserModule } from '@modules/user/user.module';
+import { postgresConnectionUri } from './configs/database.config';
+import { ContextInterceptor } from './libs/application/context/ContextInterceptor';
+import { ExceptionInterceptor } from './libs/application/interceptors/exception.interceptor';
+
+const interceptors = [
+  {
+    provide: APP_INTERCEPTOR,
+    useClass: ContextInterceptor,
+  },
+  {
+    provide: APP_INTERCEPTOR,
+    useClass: ExceptionInterceptor,
+  },
+];
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    RequestContextModule,
+    SlonikModule.forRoot({
+      connectionUri: postgresConnectionUri,
+    }),
+
+    // Modules
+    UserModule,
+  ],
+  controllers: [],
+  providers: [...interceptors],
 })
 export class AppModule {}
